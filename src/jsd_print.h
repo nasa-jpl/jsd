9d16a011d98c194620d580bf1041d1a450141114
David Kim <david.inkyu.kim@jpl.nasa.gov>
2023-03-28 14:26:24 -0700

    Added EgdFault function to be called to purge outcoming command in case of fault

--- src/jsd_print.h
+++ src/jsd_print.h
@@ -5,32 +5,29 @@
 extern "C" {
 #endif
 
-#include <jsd/jsd_time.h>
+#include "jsd/jsd_time.h"
+#include "cfw/cfw_log.h"
+
 #include <stdio.h>
 
 #ifdef DEBUG
-#define MSG_DEBUG(M, ...)                                                     \
-  fprintf(stderr, "[ DEBUG ] [%lf] (%s:%d) " M "\n", jsd_time_get_time_sec(), \
-          __FILE__, __LINE__, ##__VA_ARGS__)
+#define MSG_DEBUG(M, ...) \
+  cfw_log_print(CFW_DEBUG, __FILE__, __LINE__, M, ##__VA_ARGS__)
 #else
 #define MSG_DEBUG(M, ...)
 #endif
 
-#define MSG(M, ...)                                                           \
-  fprintf(stderr, "[ INFO  ] [%lf] (%s:%d) " M "\n", jsd_time_get_time_sec(), \
-          __FILE__, __LINE__, ##__VA_ARGS__)
+#define MSG(M, ...) \
+  cfw_log_print(CFW_INFO, __FILE__, __LINE__, M, ##__VA_ARGS__)
 
-#define WARNING(M, ...)                                               \
-  fprintf(stderr, "\033[1;33m[ WARN  ] [%lf] (%s:%d) " M "\033[0m\n", \
-          jsd_time_get_time_sec(), __FILE__, __LINE__, ##__VA_ARGS__)
+#define WARNING(M, ...) \
+  cfw_log_print(CFW_WARN, __FILE__, __LINE__, M, ##__VA_ARGS__)
 
-#define ERROR(M, ...)                                                 \
-  fprintf(stderr, "\033[1;31m[ ERROR ] [%lf] (%s:%d) " M "\033[0m\n", \
-          jsd_time_get_time_sec(), __FILE__, __LINE__, ##__VA_ARGS__)
+#define ERROR(M, ...) \
+  cfw_log_print(CFW_ERROR, __FILE__, __LINE__, M, ##__VA_ARGS__)
 
-#define SUCCESS(M, ...)                                               \
-  fprintf(stderr, "\033[1;32m[SUCCESS] [%lf] (%s:%d) " M "\033[0m\n", \
-          jsd_time_get_time_sec(), __FILE__, __LINE__, ##__VA_ARGS__)
+#define SUCCESS(M, ...) \
+  cfw_log_print(CFW_INFO, __FILE__, __LINE__, M, ##__VA_ARGS__)
 
 #ifdef __cplusplus
 }
