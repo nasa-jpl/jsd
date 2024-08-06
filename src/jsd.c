0118c757ce2d10ca27144bbfb0c848b12d5c99f2
William Reid <william.reid@jpl.nasa.gov>
2024-08-05 17:11:38 -0700

    Moving variable

--- src/jsd.c
+++ src/jsd.c
@@ -234,7 +234,7 @@ bool jsd_init(jsd_t* self, const char* ifname, uint8_t enable_autorecovery) {
 bool jsd_all_slaves_operational(jsd_t* self) {
   int slave;
   bool all_slaves_operational = true;
-
+  uint8_t currentgroup = 0;  // only 1 rate group in JSD currently
   /* one or more slaves may not be responding */
   for (slave = 1; slave <= *self->ecx_context.slavecount; slave++) {
     if (self->ecx_context.slavelist[slave].group != currentgroup) continue;
@@ -262,7 +262,6 @@ bool jsd_all_slaves_operational(jsd_t* self) {
 }
 
 void jsd_inspect_context(jsd_t* self) {
-  uint8_t currentgroup = 0;  // only 1 rate group in JSD currently
   ec_state bus_state = jsd_get_device_state(self, 0);
 
   /* first check if the jsd bus is operational so we can get more info */
