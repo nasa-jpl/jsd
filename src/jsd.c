4b31957f87e809546e33f0db050c450dc6eeb512
Kristopher T Wehage <kwehage@citadel-chamber.jpl.nasa.gov>
2024-12-19 14:21:06 -0800

    Add jsd init to jsd.c for el3314

--- src/jsd.c
+++ src/jsd.c
@@ -15,6 +15,7 @@
 #include "jsd/jsd_el3162.h"
 #include "jsd/jsd_el3202.h"
 #include "jsd/jsd_el3208.h"
+#include "jsd/jsd_el3314.h"
 #include "jsd/jsd_el3318.h"
 #include "jsd/jsd_el3356.h"
 #include "jsd/jsd_el3602.h"
@@ -468,6 +469,8 @@ const char* jsd_driver_type_to_string(jsd_driver_type_t driver_type) {
       return "JSD_DRIVER_TYPE_EL3202";
     case JSD_DRIVER_TYPE_EL3208:
       return "JSD_DRIVER_TYPE_EL3208";
+    case JSD_DRIVER_TYPE_EL3314:
+      return "JSD_DRIVER_TYPE_EL3314";
     case JSD_DRIVER_TYPE_EL3318:
       return "JSD_DRIVER_TYPE_EL3318";
     case JSD_DRIVER_TYPE_EL3356:
@@ -587,6 +590,9 @@ bool jsd_driver_is_compatible_with_product_code(jsd_driver_type_t driver_type,
     case JSD_DRIVER_TYPE_EL3202:
       is_compatible = jsd_el3202_product_code_is_compatible(product_code);
       break;
+    case JSD_DRIVER_TYPE_EL3314:
+      is_compatible = jsd_el3314_product_code_is_compatible(product_code);
+      break;
     case JSD_DRIVER_TYPE_EL3318:
       is_compatible = jsd_el3318_product_code_is_compatible(product_code);
       break;
@@ -653,6 +659,9 @@ bool jsd_init_single_device(jsd_t* self, uint16_t slave_id) {
       break;
     case JSD_DRIVER_TYPE_EL3104:
       return jsd_el3104_init(self, slave_id);
+      break;
+    case JSD_DRIVER_TYPE_EL3314:
+      return jsd_el3314_init(self, slave_id);
       break;
     case JSD_DRIVER_TYPE_EL3318:
       return jsd_el3318_init(self, slave_id);
